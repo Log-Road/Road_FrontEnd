1a32c86028fe78c1f50bd0f05dabeec88784741e
limda00 <dylim7715@gmail.com>
2024-07-08 14:52:53 +0900

    ✨:: 요소 선택하기 기능 추가

--- src/components/ListSelectValue.tsx
+++ src/components/ListSelectValue.tsx
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
 interface PropsType {
     value?: Array<string>
 }
 
-export default function ListSelectValue({value} : PropsType) {
+export default function ListSelectValue({ value }: PropsType) {
+
+    const [selectedValue, setSelectedValue] = useState(value ? value[0] : undefined)
+
     return (
         <Container>
             {value && value.map((value) => (
-                <Content>{value}</Content>
+                <Content
+                    selected={value === selectedValue}
+                    onClick={() => setSelectedValue(value)}
+                >
+                    {value}
+                </Content>
             ))}
         </Container>
     )
@@ -20,11 +28,12 @@ display: flex;
 gap: 10px;
 `
 
-const Content = styled.div`
-border: 1px solid #BABABA;
-border-radius: 20px;
-padding: 8px 2%;
-font-family: "Pretendard-Regular";
-font-size: 0.9rem;
-color: #8C8C8C;
-`
+const Content = styled.div<{ selected: boolean }>`
+    border: 1px solid ${({ selected }) => (selected ? "#1F62E4" : "#BABABA")};
+    border-radius: 20px;
+    padding: 8px 2%;
+    font-family: "Pretendard-Regular";
+    font-size: 0.9rem;
+    color: ${({ selected }) => (selected ? "#1F62E4" : "#BABABA")};
+    cursor: pointer;
+`;
