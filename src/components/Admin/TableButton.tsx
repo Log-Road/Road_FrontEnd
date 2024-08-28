b73a76f0a3912f9a92c8d36e62f0f26c61a67173
limda00 <dylim7715@gmail.com>
2024-08-28 21:41:26 +0900

    💄 :: 버튼 컴포넌트 props 색상 지정

--- src/components/Admin/TableButton.tsx
+++ src/components/Admin/TableButton.tsx
@@ -3,19 +3,25 @@ import styled from "styled-components";
 
 interface PropsType {
     text?: string
+    color?: string
 }
 
-export default function TableButton({text} : PropsType) {
+interface ButtonProps {
+    color?: string
+}
+
+export default function TableButton({text, color} : PropsType) {
     return (
-        <Button>{text}</Button>
+        <Button color={color}>{text}</Button>
     )
 }
 
-const Button = styled.button`
+const Button = styled.button<ButtonProps>`
 padding: 4px 20px;
 border: none;
 border-radius: 12px;
 font-family: "Pretendard-Medium";
 font-size: 14px;
-color: #474747;
+color: ${(props) => (props.color? "#ffffff" : "#474747")};
+background-color: ${(props) => props.color || "#F2F2F2"};
 `
