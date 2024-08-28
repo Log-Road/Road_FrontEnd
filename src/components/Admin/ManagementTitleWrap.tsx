5ad6bb9b825f43574ec1893be46f2d32a4c8424d
limda00 <dylim7715@gmail.com>
2024-08-28 21:43:24 +0900

    💄 :: 페이지 별 옵션 버튼 지정

--- src/components/Admin/ManagementTitleWrap.tsx
+++ src/components/Admin/ManagementTitleWrap.tsx
@@ -3,19 +3,34 @@ import styled from "styled-components";
 import DocumentButton from "../../components/Admin/DocumentButton";
 import { Plus, Exchange } from "../../assets/Admin";
 
-export default function ManagementTitleWrap() {
+interface PropsType {
+    id?: string,
+    title?: string
+    info?: string
+}
+
+export default function ManagementTitleWrap({ id, title, info }: PropsType) {
 
     return (
-        <Container>
-            <TitleAndInfo>
-                <Title>인원 정보 관리</Title>
-                <SubInfo>학생 정보를 편집하고 관리할 수 있어요</SubInfo>
-            </TitleAndInfo>
-            <TitleAndInfo>
-                <DocumentButton icon={Plus} text="인원 문서로 추가하기" />
-                <DocumentButton icon={Exchange} text="인원 문서로 수정하기" />
-            </TitleAndInfo>
-        </Container>
+        <>
+            <Container>
+                <TitleAndInfo>
+                    <Title>{title}</Title>
+                    <SubInfo>{info}</SubInfo>
+                </TitleAndInfo>
+                <TitleAndInfo>
+                    {id === "person" && (
+                        <>
+                            <DocumentButton icon={Plus} text="인원 문서로 추가하기" />
+                            <DocumentButton icon={Exchange} text="인원 문서로 수정하기" />
+                        </>
+                    )}
+                    {id === "contest" && (
+                        <DocumentButton icon={Plus} text="대회 추가하기" />
+                    )}
+                </TitleAndInfo>
+            </Container>
+        </>
     )
 }
 
@@ -36,7 +51,7 @@ font-size: 28px;
 `
 
 const SubInfo = styled.p`
-font-family: "Pretendard-Semibold";
+font-family: "Pretendard-Medium";
 font-size: 14px;
 color: #8C8C8C;
 
