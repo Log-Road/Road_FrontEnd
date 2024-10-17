ee0f647d7bb965ea594bd926b9d3f501170745ad
limda00 <dylim7715@gmail.com>
2024-10-17 19:36:23 +0900

    ✨ :: 리액트 쿼리 설정 추가

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,27 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { GlobalStyle } from './style/GlobalStyle';
 import "./fonts/font.css"
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      staleTime: 30000,
+      refetchInterval: 0,
+      refetchOnWindowFocus: false
+    }
+  }
+})
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <GlobalStyle/>
-    <App />
+    <QueryClientProvider client={queryClient} >
+      <GlobalStyle />
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
